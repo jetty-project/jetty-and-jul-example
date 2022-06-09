534f5165d63bb6b4059974349fa009bef7bd5e97
Joakim Erdfelt <joakim.erdfelt@gmail.com>
2022-06-09 15:45:10 -0500

    Updates for Jetty 11.0.9

--- jetty-jul-test-webapp/src/main/java/org/eclipse/jetty/examples/logging/EchoFormServlet.java
+++ jetty-jul-test-webapp/src/main/java/org/eclipse/jetty/examples/logging/EchoFormServlet.java
@@ -7,25 +7,24 @@ import java.util.Enumeration;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @SuppressWarnings("serial")
 public class EchoFormServlet extends HttpServlet
+
 {
     private static final Logger LOG = Logger.getLogger(EchoFormServlet.class.getName());
 
-    @SuppressWarnings("unchecked")
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException
     {
         resp.setContentType("text/html");
         resp.setCharacterEncoding("UTF-8");
 
-        LOG.log(Level.INFO,"Got request from {0} using {1}",
-                    new Object[] { req.getRemoteAddr(), req.getHeader("User-Agent") });
+        LOG.log(Level.INFO, "Got request from {0} using {1}",
+            new Object[]{req.getRemoteAddr(), req.getHeader("User-Agent")});
 
         PrintWriter out = null;
 
